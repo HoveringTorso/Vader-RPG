124dcc70614449417e4a84e23a9e81a06aa30e15
Jake Campbell <jakecmail@gmail.com>
2019-09-07 03:50:11 -0400

    Completed select player, select enemy, and fight functionality

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,50 +1,51 @@
 $(document).ready(function() {
   var vaders = [
     {
-      name: "Wimpy Vader",
+      name: "Wimpy",
       identification: "0",
       image: "assets/images/wimpy.jpg",
-      baseHealth: 10,
-      currentHealth: 10,
-      baseAttack: 5,
-      currentAttack: 5,
+      baseHealth: 32,
+      currentHealth: 32,
+      baseAttack: 10,
+      currentAttack: 10,
       counterAttack: 5
     },
     {
-      name: "Whiny Vader",
+      name: "Whiny",
       identification: "1",
       image: "assets/images/whiny.jpg",
-      baseHealth: 20,
-      currentHealth: 20,
-      baseAttack: 10,
-      currentAttack: 10,
+      baseHealth: 25,
+      currentHealth: 25,
+      baseAttack: 12,
+      currentAttack: 12,
+      counterAttack: 12
+    },
+    {
+      name: "Wicked",
+      identification: "2",
+      image: "assets/images/wicked.jpg",
+      baseHealth: 33,
+      currentHealth: 33,
+      baseAttack: 12,
+      currentAttack: 12,
       counterAttack: 10
     },
     {
-      name: "Wicked Vader",
-      identification: "2",
-      image: "assets/images/wicked.jpg",
-      baseHealth: 50,
-      currentHealth: 50,
-      baseAttack: 20,
-      currentAttack: 20,
-      counterAttack: 20
-    },
-    {
-      name: "Wise Vader",
+      name: "Wise",
       identification: "3",
       image: "assets/images/wise.jpg",
-      baseHealth: 100,
-      currentHealth: 100,
-      baseAttack: 1,
-      currentAttack: 1,
-      counterAttack: 1
+      baseHealth: 60,
+      currentHealth: 60,
+      baseAttack: 5,
+      currentAttack: 5,
+      counterAttack: 2
     }
   ];
 
   var playerVader = {};
   var playerSelected = false;
   var enemyVader = {};
+  var enemySelected = false;
 
   // Reorder identification numbers of vaders array to acommodate any changes to which vaders were added/removed
   function reorderVaderIDs() {
@@ -55,17 +56,46 @@ $(document).ready(function() {
 
   // Update a card with the necessay vader info
   function updateCard(cardID, vader) {
-      // get card with cardID and populate it with vader object data
-    $(cardID).html();
+    // get card with cardID and populate it with vader object data
+    $("#" + cardID).html(
+      '<img src="' +
+        vader.image +
+        '" class="card-img-top" alt="Vader Image" /><div class="card-body"><h5 class="card-title">' +
+        vader.name +
+        ' </h5><p class="card-text"><span class="label">Attack: </span><span>' +
+        vader.currentAttack +
+        '</span><br /><span class="label">Counter-attack: </span><span>' +
+        vader.counterAttack +
+        '</span><br /><span class="label">Health: </span><span>' +
+        vader.currentHealth +
+        "/" +
+        vader.baseHealth +
+        '</span></p><div class="progress"><div class="progress-bar progress-bar-striped bg-danger progress-bar-animated" role="progressbar" style="width: ' +
+        (vader.currentHealth / vader.baseHealth) * 100 +
+        '%"></div></div></div>'
+    );
+
+    $("#" + cardID).addClass("selectable");
   }
 
   // Redraw all cards with updated info
   function redrawAllCards() {
-      for (var i = 0; i < vaders.length; i++) {
-          updateCard(i, vaders[i])
-      }
+    $("#top-deck").empty();
+    for (var i = 0; i < vaders.length; i++) {
+      $("#top-deck").append('<div class="card" id="' + i + '"></div>');
+      updateCard(i, vaders[i]);
+    }
+    if (playerSelected === true) {
       updateCard("player-vader", playerVader);
-      updateCare("enemy-vader", enemyVader);
+      $("#player-vader").removeClass("selectable");
+    }
+    if (enemySelected === true) {
+      updateCard("enemy-vader", enemyVader);
+      $("#attack-button").html(
+        '<button class="btn"><img src="assets/images/clash.png" /></button>'
+      );
+      $("#enemy-vader").removeClass("selectable");
+    }
   }
 
   // Select an enemy to pull out of the vaders array to the defender area
@@ -79,20 +109,69 @@ $(document).ready(function() {
     // If the enemyVader wasn't defeated, subtract the playerVader's HP by enemyVader's counterAttack
   }
 
-  console.log(vaders);
+  redrawAllCards();
+
   // When any of the vader cards are click at the beginning of the game, assign that vader to the player
-  $(".selectable").click(function() {
+  $(document).on("click", ".selectable", function(event) {
     if (playerSelected === false) {
-      console.log(vaders);
-      console.log("These are current vaders ^");
-      $(".selectable").removeClass(".selectable");
       var id = $(this).attr("id");
       playerVader = vaders[id];
-      playerVader.identification.replace(playerVader.identification, "player-vader");
+      playerVader.identification.replace(
+        playerVader.identification,
+        "player-vader"
+      );
+      playerSelected = true;
       vaders.splice(id, 1);
       reorderVaderIDs();
-      console.log(playerVader);
-      console.log(vaders);
+      redrawAllCards();
+    } else if (playerSelected === true) {
+      var id = $(this).attr("id");
+      enemyVader = vaders[id];
+      enemyVader.identification.replace(
+        enemyVader.identification,
+        "enemy-vader"
+      );
+      enemySelected = true;
+      vaders.splice(id, 1);
+      reorderVaderIDs();
+      redrawAllCards();
+      console.log("Enemy Vader is " + enemyVader.name);
+    }
+  });
+
+  $(document).on("click", "#attack-button", function(event) {
+    enemyVader.currentHealth -= playerVader.currentAttack;
+    playerVader.currentAttack += playerVader.baseAttack;
+    playerVader.currentHealth -= enemyVader.counterAttack;
+    if (enemyVader.currentHealth <= 0) {
+      $("#enemy-vader").empty();
+      enemyVader = null;
+      $("#attack-button").empty();
+      enemySelected = false;
+    }
+    redrawAllCards();
+
+    if (playerVader.currentHealth <= 0) {
+      $("#bottom-row").empty();
+      $("#bottom-row").html('<h1 style="text-align:center;">Game Over!<h1>');
+      $(".card").removeClass("selectable");
+    } else if (vaders.length === 0 && enemyVader === null) {
+      $("#attack-button").empty();
+      switch (playerVader.name) {
+        case "Wimpy":
+          $("#attack-button").html("<h1>You're a person, and you're name is Anakin!<h1>");
+          break;
+        case "Whiny":
+          $("#attack-button").html("<h1>You hate sand!<h1>");
+          break;
+        case "Wicked":
+          $("#attack-button").html("<h1>You know the power of the dark side!<h1>");
+          break;
+        case "Wise":
+          $("#attack-button").html("<h1>Luke was right about you!<h1>");
+          break;
+      }
+      $(".card").removeClass("selectable");
     }
   });
 });
